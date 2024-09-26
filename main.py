707b51280788063aab093c8e076708a08cfe160a
ChrisKod007 <cdbm001@gmail.com>
2024-09-26 18:13:25 -0500

    Create main.py
    Primera versión de generador de contraseñas

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+caracteres_disponibles = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+def generar_contrasena(longitud):
+    contrasena = ''.join(random.choice(caracteres_disponibles) for _ in range(longitud))
+    return contrasena
+
+def main():
+    longitud = int(input("Ingrese la longitud de la contraseña: "))
+    contrasena = generar_contrasena(longitud)
+    print("Contraseña generada:", contrasena)
+
+if __name__ == "__main__":
+    main()
